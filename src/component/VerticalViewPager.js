afb5c8ad25421efd5bfa97341b865ba8462a9869
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-10-02 11:47:28 +0800

    update

--- src/component/VerticalViewPager.js
+++ src/component/VerticalViewPager.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  ScrollView
+    ScrollView
 } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -18,146 +18,193 @@ const SCROLL_THRESHOLD = 0.2;
  */
 class VerticalViewPager extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this._contentOffset = null;
-    this._layout = null;
-    this.state = {
-      scrollEnabled: true
-    };
-  }
-
-  scrollTo({ x, y, animated }) {
-    this.scrollview.scrollTo({ x, y, animated });
-  }
-
-  onScrollBeginDrag(e) {
-    // record starting points
-    this._setStartOffset(e.nativeEvent.contentOffset);
-    _.invoke(this.props, 'onScrollBeginDrag', e);
-  }
-
-  onScrollEndDrag (e) {
-    // record ending points
-    this._setEndOffset(e.nativeEvent.contentOffset);
-    // calculate the offset the user scrolls
-    this._calculateOffsets();
-    _.invoke(this.props, 'onScrollEndDrag', e);
-  }
-
-  onMomentumScrollEnd(e) {
-    _.invoke(this.props, 'onMomentumScrollEnd', e);
-    this.setState({
-      scrollEnabled: true
-    });
-  }
-
-  nextPage() {
-    // calculate next y offset value
-    const { height } = this._layout;
-    const { y: startY } = this._startContentOffset;
-    const nextYOffset = startY + height;
-    // scrollTo that point
-    this.scrollTo({ y: nextYOffset });
-    this.setState({
-      scrollEnabled: false
-    });
-  }
-
-  prevPage() {
-    // calcullate prev page's y offset value
-    const { height } = this._layout;
-    const { y: startY } = this._startContentOffset;
-    const nextYOffset = startY - height;
-    // scrollTo that point
-    this.scrollTo({ y: nextYOffset });
-    this.setState({
-      scrollEnabled: false
-    });
-  }
-
-  _setStartOffset(startOffset) {
-    this._startContentOffset = startOffset;
-    this._endContentOffset = null;
-  }
-
-  _setEndOffset(endOffset) {
-    this._endContentOffset = endOffset;
-  }
-
-  _calculateOffsets() {
-    const { height } = this._layout;
-    const { y: startY } = this._startContentOffset;
-    const { y: endY } = this._endContentOffset;
-    if ((endY - startY) > height * SCROLL_THRESHOLD) {
-      this.nextPage();
+        this._scrolling = false;
+        this._enableScrollTimer = null;
+        this._layout = null;
+        this._contentOffset = null;
+        this.state = {
+            scrollEnabled: true
+        };
     }
-    else if ((endY - startY) < -height * SCROLL_THRESHOLD) {
-      this.prevPage();
+
+    scrollTo({x, y, animated}) {
+        console.log('scrollTo');
+        console.log({y, animated});
+        this.scrollview.scrollTo({x, y, animated});
     }
-    else {
-      this.scrollTo({ y: startY });
+
+    onScrollBeginDrag(e) {
+        console.log('onScrollBeginDrag');
+        console.log(e.nativeEvent);
+        // record starting points
+        this._setStartOffset(e.nativeEvent.contentOffset);
+        this._scrolling = true;
+        _.invoke(this.props, 'onScrollBeginDrag', e);
     }
-  }
 
-  _refScrollView(scrollview) {
-    this.scrollview = scrollview;
-  }
-
-  _onLayout(e) {
-    this._layout = e.nativeEvent.layout;
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { contentOffset } = this.props;
-    const { contentOffset: nextContentOffset} = nextProps;
-    if (contentOffset != nextContentOffset) {
-      this.scrollTo({ ...nextContentOffset, animated: false });
+    onScrollEndDrag(e) {
+        console.log('onScrollEndDrag');
+        console.log(e.nativeEvent)
+        // record ending points
+        this._setEndOffset(e.nativeEvent.contentOffset);
+        this._scrolling = false;
+        // calculate the offset the user scrolls
+        this._calculateOffsets();
+        _.invoke(this.props, 'onScrollEndDrag', e);
     }
-  }
 
-  render() {
-    const {
-      contentContainerStyle,
-      style
-    } = this.props;
-    return (
-      <ScrollView
-        ref={ scrollview => this._refScrollView(scrollview) }
-        onLayout={ e => this._onLayout(e) }
-        horizontal={false}
-        style={style}
-        scrollEnabled={this.state.scrollEnabled}
-        onScrollBeginDrag={ e => this.onScrollBeginDrag(e) }
-        onMomentumScrollEnd={ e => this.onMomentumScrollEnd(e) }
-        onScrollEndDrag ={ e => this.onScrollEndDrag(e) }
-        contentContainerStyle={contentContainerStyle}>
-        {this.props.children}
-      </ScrollView>
-    )
-  }
+    onMomentumScrollEnd(e) {
+        console.log('onMomentumScrollEnd');
+        console.log(e.nativeEvent);
+        _.invoke(this.props, 'onMomentumScrollEnd', e);
+        this.setState({
+            scrollEnabled: true
+        });
+    }
+
+    nextPage() {
+        console.log('nextPage');
+        // calculate next y offset value
+        const {height} = this._layout;
+        const {y: startY} = this._startContentOffset;
+        const nextYOffset = startY + height;
+        // scrollTo that point
+        this.scrollTo({y: nextYOffset, animated: true});
+        this._contentOffset = {x: 0, y: nextYOffset}
+        this.setState({
+            scrollEnabled: false
+        });
+    }
+
+    prevPage() {
+        console.log('prevPage');
+        // calcullate prev page's y offset value
+        const {height} = this._layout;
+        const {y: startY} = this._startContentOffset;
+        const nextYOffset = startY - height;
+        // scrollTo that point
+        this.scrollTo({y: nextYOffset, animated: true});
+        this._contentOffset = {x: 0, y: nextYOffset};
+        this.setState({
+            scrollEnabled: false
+        });
+    }
+
+    onScroll(e) {
+        if (!this._scrolling) {
+            this._startEnableScrollTimer();
+        }
+    }
+
+    _setStartOffset(startOffset) {
+        this._startContentOffset = startOffset;
+        this._endContentOffset = null;
+    }
+
+    _setEndOffset(endOffset) {
+        this._endContentOffset = endOffset;
+    }
+
+    _calculateOffsets() {
+        const {height} = this._layout;
+        const {y: startY} = this._startContentOffset;
+        const {y: endY} = this._endContentOffset;
+        if ((endY - startY) > height * SCROLL_THRESHOLD) {
+            this.nextPage();
+        }
+        else if ((endY - startY) < -height * SCROLL_THRESHOLD) {
+            this.prevPage();
+        }
+        else {
+            this.scrollTo({y: startY});
+        }
+    }
+
+    _refScrollView(scrollview) {
+        this.scrollview = scrollview;
+    }
+
+    _onLayout(e) {
+        this._layout = e.nativeEvent.layout;
+    }
+
+    _startEnableScrollTimer() {
+        clearTimeout(this._enableScrollTimer);
+        this._enableScrollTimer = setTimeout(() => {
+            console.log('enableScrollTimer fired');
+            this.setState({
+                scrollEnabled: true
+            });
+            // Whether ScrollView onMomentumScrollEnd fire or not is depend on user interaction, so the event will not fire every time it scrolls. Here mimic
+            // the event to ensure every time done scrolling will fire onMomentumScrollEnd event.
+            const e = {
+                nativeEvent: {
+                    contentOffset: this._contentOffset
+                }
+            };
+            console.log(e);
+            _.invoke(this.props, 'onMomentumScrollEnd', e);
+        }, 25);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const {contentOffset} = this.props;
+        const {contentOffset: nextContentOffset} = nextProps;
+        if (contentOffset != nextContentOffset) {
+            this.scrollTo({...nextContentOffset, animated: false});
+        }
+    }
+
+    render() {
+        const {
+            contentContainerStyle,
+            style
+        } = this.props;
+        return (
+            <ScrollView
+                ref={scrollview => this._refScrollView(scrollview)}
+                onLayout={e => this._onLayout(e)}
+                horizontal={false}
+                style={style}
+                scrollEnabled={this.state.scrollEnabled}
+                onScrollBeginDrag={e => this.onScrollBeginDrag(e)}
+                onMomentumScrollEnd={e => this.onMomentumScrollEnd(e)}
+                onScrollEndDrag={e => this.onScrollEndDrag(e)}
+                onScroll={e => this.onScroll(e)}
+                contentContainerStyle={contentContainerStyle}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}>
+                {this.props.children}
+            </ScrollView>
+        )
+    }
 }
 
 VerticalViewPager.propTypes = {
-  // style of page container
-  contentContainerStyle: PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.object
-  ]),
-  contentOffset: PropTypes.object,
-  // onScrollBeginDrag(nativeEvent)
-  // a.k.a onScrollBegin
-  onScrollBeginDrag: PropTypes.func,
-  // onMomentumScrollEnd(nativeEvent)
-  // nativeEvent.nativeEvent.contentOffset or nativeEvent.nativeEvent.position must exists
-  // a.k.a onScrollEnd
-  onMomentumScrollEnd: PropTypes.func,
-  // onScrollEndDrag(nativeEvent)
-  // nativeEvent.nativeEvent.contentOffset must exists
-  // a.k.a. onScrollEndDrag
-  onScrollEndDrag: PropTypes.func,
-  style: PropTypes.object
+    // style of page container
+    contentContainerStyle: PropTypes.oneOfType([
+        PropTypes.object,
+        PropTypes.array
+    ]),
+    contentOffset: PropTypes.object,
+    // onScrollBeginDrag(nativeEvent)
+    // a.k.a onScrollBegin
+    onScrollBeginDrag: PropTypes.func,
+    // onMomentumScrollEnd(nativeEvent)
+    // nativeEvent.nativeEvent.contentOffset or nativeEvent.nativeEvent.position must exists
+    // a.k.a onScrollEnd
+    onMomentumScrollEnd: PropTypes.func,
+    // onScrollEndDrag(nativeEvent)
+    // nativeEvent.nativeEvent.contentOffset must exists
+    // a.k.a. onScrollEndDrag
+    onScrollEndDrag: PropTypes.func,
+    style: PropTypes.oneOfType([
+        PropTypes.object,
+        PropTypes.array
+    ])
 };
 
 export default VerticalViewPager;
